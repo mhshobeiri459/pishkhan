1921142c4ec31adefadf63d8da8160b252b9580c
unknown <mhshobeiri459@gmail.com>
2021-03-20 09:03:56 -0700

    models defined and registered on the admin site

--- pishkhanapp/admin.py
+++ pishkhanapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from pishkhanapp.models import *
 # Register your models here.
+admin.site.register(userfile)
+admin.site.register(medicalForm)
+admin.site.register(violation)
+admin.site.register(service)
+admin.site.register(resultreq)
--- pishkhanapp/models.py
+++ pishkhanapp/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+CHOICES = [
+    ('DrivingLiscense', 'DrivingLiscense'),
+    ('Passport', 'Passport'),
+    ('SSCard', 'SSCard'),
+    ('Violation', 'Violation'),
+]
+length = 25
 # Create your models here.
+class userfile(models.Model):
+    firstname = models.CharField(max_length=length, blank=True, null=True)
+    lastname = models.CharField(max_length=length, blank=True, null=True)
+    ssn = models.IntegerField()
+    birthdate = models.DateField(auto_now=False, auto_now_add=False)
+    birthlocation = models.CharField(max_length=length, blank=True, null=True)
+    user = models.OneToOneField(User , on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.firstname
+
+
+class medicalForm(models.Model):
+    firstname = models.CharField(max_length=length, blank=True, null=True)
+    lastname = models.CharField(max_length=length, blank=True, null=True)
+    ssn = models.IntegerField()
+    birthdate = models.DateField(auto_now=False, auto_now_add=False)
+    result = models.FileField( upload_to=None, max_length=100)
+
+    
+
+    class Meta:
+        verbose_name = ("medicalform")
+        verbose_name_plural = ("medicalforms")
+
+    def __str__(self):
+        return self.firstname
+
+    def get_absolute_url(self):
+        return reverse("medicalform_detail", kwargs={"pk": self.pk})
+
+class violation(models.Model):
+    name = models.CharField(max_length=length, blank=True, null=True, choices=CHOICES,)
+    policenum = models.IntegerField()
+    carnum = models.IntegerField()
+    filenum = models.ForeignKey("pishkhanapp.userfile",  on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ("violation")
+        verbose_name_plural = ("violations")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("violation_detail", kwargs={"pk": self.pk})
+
+class service(models.Model):
+    typeofservice  = models.CharField(max_length=length, blank=True, null=True)
+    servicedate = models.DateField(auto_now=False, auto_now_add=False)    
+    filenum = models.ForeignKey("pishkhanapp.userfile",  on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ("service")
+        verbose_name_plural = ("services")
+
+    def __str__(self):
+        return self.typeofservice
+
+    def get_absolute_url(self):
+        return reverse("service_detail", kwargs={"pk": self.pk})
+
+
+class resultreq(models.Model):
+    filenum = models.ForeignKey("pishkhanapp.userfile", on_delete=models.CASCADE)
+    regnum = models.IntegerField()
+    result = models.FileField( upload_to=None, max_length=100)
+    
+
+    class Meta:
+        verbose_name = ("resultreq")
+        verbose_name_plural = ("resultreqs")
+
+    def get_absolute_url(self):
+        return reverse("resultreq_detail", kwargs={"pk": self.pk})
